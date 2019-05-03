1ab973370fff49c4c5aed73eff3dcdba036a1445
ABM97 <viragesud97@gmail.com>
2019-05-03 21:58:35 +0200

    test

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const server = express();
 const PORT = 3000;
-const pg = require("pg");
-pg.connect("postgres://postgres:z@localhost:5432/practicedocker");
+const db = require("./server");
+
+const connectSequelize = async () => {
+  try {
+    await db.authenticate();
+    console.log("Successfully connected to the database");
+  } catch (e) {
+    console.log("error");
+  }
+};
+
+connectSequelize();
+
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
 server.get("/", (req, res) => res.status(200).send("hello"));
